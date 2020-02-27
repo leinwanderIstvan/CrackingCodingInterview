7d9016ae5fbc7fdfbcf47a25fefd3104fcb01b73
leinwanderIstvan <coolcodebasic@gmail.com>
2020-02-27 15:28:54 +0100

    Approach of Is_Unique problem is implemented

---
+++ Is_Unique_02/Is_Unique_02/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Is_Unique_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string inputStr = "";
+            if (isUniquCharacters(inputStr.ToLower()) == true)
+            {
+                Console.WriteLine("This input has just unique characters!");
+            }
+            else
+            {
+                Console.WriteLine("This input has duplicates!");
+            }
+
+            Console.ReadLine();
+            
+        }
+
+        static bool isUniquCharacters(string word) 
+        {
+            char[] chars = word.ToCharArray();
+
+            Array.Sort(chars);
+
+            for (int i = 0; i < chars.Length - 1; i++)
+            {
+                if (chars[i] != chars[i + 1])
+                {
+                    continue;
+                }
+                else 
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
+
+        }
+
+
+    }
+}
