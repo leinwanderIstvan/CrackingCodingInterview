7c681b836e5a537a9a98e5868232b1d191730438
leinwanderIstvan <coolcodebasic@gmail.com>
2020-02-27 17:28:56 +0100

    Is_Unique  third implemenatation

---
+++ Is_Unique_03/Is_Unique_03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Is_Unique_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = "geekforgeek";
+
+            Console.WriteLine("{0} : Unique ? {1}", input, isUniqueCharacters(input) ? "YES":"NO");
+            Console.ReadLine();
+
+            
+        }
+
+        static bool isUniqueCharacters(string str) 
+        {
+            int[] counters = new int[256];
+
+            Array.Clear(counters, 0, counters.Length -1);
+
+            foreach (var chars in str)
+            {
+                if (++counters[chars] > 1)
+                {
+                    return false;
+                }
+            }
+            
+            return true;
+        }
+    }
+}
