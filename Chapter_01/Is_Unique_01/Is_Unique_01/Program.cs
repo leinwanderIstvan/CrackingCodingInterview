5e8ee8a753685891131f5a00a698f5b391d2ecfe
leinwanderIstvan <coolcodebasic@gmail.com>
2020-02-27 14:55:55 +0100

    Approach_One implemented

---
+++ Chapter_01/Is_Unique_01/Is_Unique_01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Is_Unique_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input ="Thisisit";
+            if (uniqueCharacters(input.ToLower()) == true)
+            {
+                Console.WriteLine("The input string has all unique characters!");
+            }
+            else 
+            {
+                Console.WriteLine("The string has duplicate characters!");
+            }
+
+            Console.ReadLine();
+            
+        }
+
+        static bool uniqueCharacters(string word) 
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                for (int j = i + 1; j < word.Length; j++)
+                {
+                    if (word[i] == word[j])
+                    {
+                        
+                        return false;
+                    }
+                }
+            }
+
+
+            return true;
+        }
+    }
+}
