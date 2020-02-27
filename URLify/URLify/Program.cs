b549db5ef8de70750c9d49d28e176690b8cfdf17
leinwanderIstvan <coolcodebasic@gmail.com>
2020-02-27 20:38:03 +0100

    Replace _Space_01 implementation

---
+++ URLify/URLify/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReplaceSpace("This is a sentence !"));
+            Console.ReadLine();
+        }
+
+        public static string ReplaceSpace(string str) 
+        {
+
+            char[] strChars = str.ToCharArray();
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < strChars.Length; i++)
+            {
+                if (strChars[i] == 32)
+                {
+                    stringBuilder.Append("%20");
+                }
+                else
+                {
+                    stringBuilder.Append(strChars[i]);
+                }
+            }
+
+            return stringBuilder.ToString();
+            
+        }
+    }
+}
