74f64cf545f0e73ac8951cfdb0b1e135885c9bc6
leinwanderIstvan <coolcodebasic@gmail.com>
2020-02-27 21:27:36 +0100

    Palindrome_Permutation implemented

---
+++ Palindrome_Permutation_01/Palindrome_Permutation_01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Permutation_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "";
+
+            int[] counter = new int[128];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                counter[str[i]]++;
+            }
+
+            int count = 0;
+            for (int i = 0; i < 128; i++)
+            {
+                count += counter[i] % 2;
+            }
+
+
+            Console.WriteLine(count <= 1);
+
+
+            Console.ReadLine();
+        }
+    }
+}
