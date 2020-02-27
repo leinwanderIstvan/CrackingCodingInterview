5b75e5e2b8c06c70dbd365937aff251fd9c7e66a
leinwanderIstvan <coolcodebasic@gmail.com>
2020-02-27 21:55:28 +0100

    String_Compression first try implemented

---
+++ String_Compression/String_Compression/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Compression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "aabcccccaaa";
+            Compression(str);
+
+            Console.ReadLine();
+        }
+        
+
+        public static void Compression(string str) 
+        {
+            if (str == null || str == "")
+            {
+                Console.WriteLine("Invalid value!");
+            }
+
+            int length = str.Length;
+            for (int i = 0; i < length; i++)
+            {
+                int count = 1;
+                while (i < length - 1 && str[i] == str[i + 1])
+                {
+                    count++;
+                    i++;
+                }
+
+                Console.WriteLine("{0} : {1}", str[i], count);
+            }    
+
+            
+        }
+    }
+}
